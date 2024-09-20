01813786da1370024885cb4273039458cf0e81b6
Zeyad <zeyad.sayed15.zs@gmail.com>
2024-09-20 11:27:28 +0300

    Fixed XSS form issue

--- xss.php
+++ xss.php
@@ -2,14 +2,16 @@
 <html lang="en">
 <head>
 	<meta charset="UTF_8">
-	<title>Vulnerable XSS page</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>XSS Vulnerable page</title>
 </head>
 <body>
 	<h1>XSS Vulnerability Example</h1>
 	<from method="GET" action="xss.php>
 		<lable for="name">Enter you name:</lable>
 		<input type="text" id="name" name="name">
-		<input type="submit" value="Submit">
+		<button type="submit">Submit</button>
 	</form>
 
 	<?php
